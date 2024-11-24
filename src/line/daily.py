7af3e39a2fd9baf1e5867bd858fec293f2907816
iceice666 <iceice666@outlook.com>
2024-11-24 22:56:01 +0800

    feat (Messaging): refactor message sending to use multicast for improved user targeting

--- src/line/daily.py
+++ src/line/daily.py
@@ -2,11 +2,11 @@ import logging
 import uuid
 from datetime import datetime
 
-from src.const import SCHEDULER, QUESTIONS_DATABASE
+from src.const import SCHEDULER, QUESTIONS_DATABASE, USERS_DATABASE
 from linebot.v3.messaging import (
     ApiClient,
     BroadcastRequest, TextMessage,
-    MessagingApi, Message, ValidateMessageRequest
+    MessagingApi, MulticastRequest
 )
 from pydantic import StrictStr, StrictBool
 
@@ -39,41 +39,40 @@ def countdown() -> int:
     return (gsat_data - today).days
 
 
-def send_question():
+def send_msgs(msgs):
+    targets = USERS_DATABASE.get_enabled_users()
+    splited_targets = [targets[i:i + 500] for i in range(0, len(targets), 500)]
+
+    requests = ([
+        MulticastRequest(
+            messages=msgs,
+            to=t,
+            notificationDisabled=None,
+            customAggregationUnits=None
+        ) for t in splited_targets])
+
     with ApiClient(CONFIGURATION) as api_client:
         line_bot_api = MessagingApi(api_client)
-        result = line_bot_api.broadcast(
-            broadcast_request=BroadcastRequest(
-                messages=[
-                    TextMessage(
-                        text=StrictStr(str(countdown())),
-                        emojis=None, quoteToken=None, quickReply=None
-                    )
-                    , TextMessage(
-                        text=StrictStr(make_question()),
-                        emojis=None, quoteToken=None, quickReply=None
-                    )],
-                notificationDisabled=StrictBool(False)
-            ),
-            x_line_retry_key=StrictStr(str(uuid.uuid4())),
-        )
+        for req in requests:
+            line_bot_api.multicast(req, x_line_retry_key=StrictStr(str(uuid.uuid4())))
+
+
+def send_question():
+    send_msgs([TextMessage(
+        text=StrictStr(make_question()),
+        emojis=None,
+        quoteToken=None,
+        quickReply=None
+    )])
 
 
 def send_answer():
-    with ApiClient(CONFIGURATION) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.broadcast(
-            broadcast_request=BroadcastRequest(
-                messages=[TextMessage(
-                    text=StrictStr(make_answer()),
-                    emojis=None,
-                    quoteToken=None,
-                    quickReply=None
-                )],
-                notificationDisabled=StrictBool(False)
-            ),
-            x_line_retry_key=StrictStr(str(uuid.uuid4())),
-        )
+    send_msgs([TextMessage(
+        text=StrictStr(make_answer()),
+        emojis=None,
+        quoteToken=None,
+        quickReply=None
+    )])
 
 
 def register():
